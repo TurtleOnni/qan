89540c0167513a9ec847c60a4d1782c71a8bf74b
TurtleOnni <turtleonni@gmail.com>
2021-02-24 22:07:58 +0300

    init commit

---
+++ includes/class-qan-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/TurtleOnni
+ * @since      1.0.0
+ *
+ * @package    Qan
+ * @subpackage Qan/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Qan
+ * @subpackage Qan/includes
+ * @author     Turtle_Onni <katrik5@yandex.ru>
+ */
+class Qan_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-qan-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/TurtleOnni
+ * @since      1.0.0
+ *
+ * @package    Qan
+ * @subpackage Qan/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Qan
+ * @subpackage Qan/includes
+ * @author     Turtle_Onni <katrik5@yandex.ru>
+ */
+class Qan_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
